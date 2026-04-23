454e59c41516709a0a51c4b77ba7d2176c2c9ddd
Neto-IBS <140271677+Neto616@users.noreply.github.com>
2026-04-23 00:12:51 -0600

    Allow all

--- config/cors.php
+++ config/cors.php
@@ -18,9 +18,7 @@ return [
 
     "allowed_methods" => ["*"],
 
-    "allowed_origins" => [
-        "https://admin-tesec-front-1c4w-k7wimuve6-ivan-bss-projects.vercel.app",
-    ],
+    "allowed_origins" => ["*"],
 
     "allowed_origins_patterns" => [],
 
