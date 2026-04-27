49bb7429f39fe5a01ca67828f4e07538bba487f3
Neto-IBS <140271677+Neto616@users.noreply.github.com>
2026-04-27 09:33:29 -0600

    Changes: Add clave_sat on products data request

--- database/migrations/0001_01_01_000003_create_new_productos_table.php
+++ database/migrations/0001_01_01_000003_create_new_productos_table.php
@@ -4,28 +4,26 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::disableForeignKeyConstraints();
-        Schema::dropIfExists('productos');
-        Schema::create('productos', function (Blueprint $table) {
+        Schema::dropIfExists("productos");
+        Schema::create("productos", function (Blueprint $table) {
             $table->id();
-            $table->string('nombre');
-            $table->string('sku')->nullable();
-            $table->string('clave_sat', 10)->nullable();
-            $table->string('descripcion',255);
-            $table->string('marca')->nullable();
-            $table->text('link')->nullable();
-            $table->foreignId('id_proveedor')
-                ->constrained('proveedores');
-            $table->decimal('precio', 18, 6);
-            $table->decimal('precio_publico', 18, 6);
-            $table->tinyInteger('estatus')->default(1);
+            $table->string("nombre");
+            $table->string("sku")->nullable();
+            $table->string("clave_sat", 10)->nullable();
+            $table->string("descripcion", 255);
+            $table->string("marca")->nullable();
+            $table->text("link")->nullable();
+            $table->foreignId("id_proveedor")->constrained("proveedores");
+            $table->decimal("precio", 18, 6);
+            $table->decimal("precio_publico", 18, 6);
+            $table->tinyInteger("estatus")->default(1);
             $table->timestamps();
         });
         Schema::enableForeignKeyConstraints();
@@ -37,12 +35,12 @@ return new class extends Migration
     public function down(): void
     {
         Schema::disableForeignKeyConstraints();
-        Schema::dropIfExists('productos');
-        Schema::create('productos', function (Blueprint $table) {
+        Schema::dropIfExists("productos");
+        Schema::create("productos", function (Blueprint $table) {
             $table->id();
-            $table->string('nombre', 255);
-            $table->string('descripcion',255);
-            $table->tinyInteger('estatus')->default(1);
+            $table->string("nombre", 255);
+            $table->string("descripcion", 255);
+            $table->tinyInteger("estatus")->default(1);
             $table->timestamps();
         });
         Schema::enableForeignKeyConstraints();
